acf41914b52a1b9f8b9a6e184174c9a7273ddaa3
Jonas Thomsen <jonastoftethomsen@gmail.com>
2022-11-08 14:30:52 +0100

    Streamline the room, username and client id across the application
    Now the data is created in the RouteList and passed down to the given places where it is needed

--- src/screens/RouteList.js
+++ src/screens/RouteList.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import CallScreen from "./CallScreen";
 import HomeScreen from "./HomeScreen";
+import { v4 as uuidv4 } from 'uuid';
 
 function RouteList() {
+
+
+  function randomNum(min, max) {
+    return Math.floor(Math.random() * max) + min;
+  }
+
+  const [room, setRoom] = useState("0");
+  const [username, setUsername] = useState("" + randomNum(0, 99));
+  const [clientId] = useState(uuidv4());
+
   return (
     <Routes>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/call/:username/:room" element={<CallScreen />} />
+      <Route path="/" element={<HomeScreen room={room} setRoom={setRoom} username={username} setUsername={setUsername} clientId={clientId} />} />
+      <Route path="/call/:username/:room" element={<CallScreen clientId={clientId} />} />
     </Routes>
   );
 }
--- src/utils/WebRtcMonitor.js
+++ src/utils/WebRtcMonitor.js
@@ -1,20 +1,17 @@
 import { ClientMonitor } from '@observertc/client-monitor-js';
-//import { EventEmitter } from 'events';
-import { v4 as uuidv4 } from 'uuid';
 
 const observeRTCDomain = process.env.REACT_APP_OBSERVERTC_DOMAIN;
 const observeRTCPort = process.env.REACT_APP_OBSERVERTC_PORT;
 const observeRTCPath = process.env.REACT_APP_OBSERVERTC_PATH;
 
+if (observeRTCDomain === undefined || observeRTCPort === undefined || observeRTCPath === undefined) {
+  console.error("Missing one or more required environment variables: REACT_APP_OBSERVERTC_DOMAIN, REACT_APP_OBSERVERTC_PORT, REACT_APP_OBSERVERTC_PATH")
+}
+
 const config = {
   collectingPeriodInMs: 5000,
   samplingPeriodInMs: 10000,
   sendingPeriodInMs: 15000,
-  sampler: {
-    roomId: "0",          // Generated from the user
-    clientId: uuidv4(),   // Keep as a UUID
-    userId: uuidv4(),     // Generated from the user
-  },
   sender: {
     format: "protobuf",
     websocket: {
@@ -24,4 +21,8 @@ const config = {
   }
 };
 
-export const monitor = ClientMonitor.create(config);
+export function monitor(sampler) {
+  let x = Object.assign({}, config, sampler);
+  console.log("ObserveRTC config: ", x)
+  return ClientMonitor.create(x)
+}
