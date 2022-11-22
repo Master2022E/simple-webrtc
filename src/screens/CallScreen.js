18f1c904d946bf123ae3df3cc5a671cca4697316
Jonas Thomsen <jonastoftethomsen@gmail.com>
2022-11-22 13:41:25 +0100

    Remove access of function data that is not ready yet.

--- src/screens/CallScreen.js
+++ src/screens/CallScreen.js
@@ -201,60 +201,64 @@ function CallScreen({ clientId }) {
             type: "outbound-rtp"
           } */
 
-          const remoteInboundRtp = outboundRtp.getRemoteInboundRtp();
+          if (outboundRtp.getRemoteInboundRtp()) {
+            const remoteInboundRtp = outboundRtp.getRemoteInboundRtp();
 
-          const remoteInboundRtpStats = remoteInboundRtp.stats
 
-          const { roundTripTime, kind } = remoteInboundRtp.stats;
-          if (doLog) {
-            console.log("remoteInboundRtp (" + kind + ")", remoteInboundRtp.stats)
-          }
 
-          monitor.addExtensionStats({
-            type: "REMOTE_IN_BOUND_RTC",
-            payload: JSON.stringify({
-              "stats": remoteInboundRtpStats
-            }),
-          })
+            const remoteInboundRtpStats = remoteInboundRtp.stats
 
-          if (kind === "video") {
-            setVideoRTT(roundTripTime)
-            /* stats contains: {
-              codecId: "57e44166",
-              fractionLost: 0,
-              id: "49881f3",
-              jitter: 0.004,
-              kind: "video",
-              localId: "93489802",
-              mediaType: "video",
-              packetsLost: 0,
-              packetsReceived: 97,
-              roundTripTime: 0.026,
-              roundTripTimeMeasurements: 40,
-              ssrc: 3647127630,
-              timestamp: undefined,
-              totalRoundTripTime: 0.776,
-              type: "remote-inbound-rtp"
-            } */
-          } else if (kind === "audio") {
-            setAudioRTT(roundTripTime)
-            /* stats contains: {
-              codecId: "ce84b808",
-              fractionLost: 0,
-              id: "1c3c052d",
-              jitter: 0.004,
-              kind: "audio",
-              localId: "e9eea0d0",
-              mediaType: "audio",
-              packetsLost: 0,
-              packetsReceived: 1191,
-              roundTripTime: 0.021,
-              roundTripTimeMeasurements: 20,
-              ssrc: 342435103,
-              timestamp: undefined,
-              totalRoundTripTime: 0.376,
-              type: "remote-inbound-rtp"
-            } */
+            const { roundTripTime, kind } = remoteInboundRtp.stats;
+            if (doLog) {
+              console.log("remoteInboundRtp (" + kind + ")", remoteInboundRtp.stats)
+            }
+
+            monitor.addExtensionStats({
+              type: "REMOTE_IN_BOUND_RTC",
+              payload: JSON.stringify({
+                "stats": remoteInboundRtpStats
+              }),
+            })
+
+            if (kind === "video") {
+              setVideoRTT(roundTripTime)
+              /* stats contains: {
+                codecId: "57e44166",
+                fractionLost: 0,
+                id: "49881f3",
+                jitter: 0.004,
+                kind: "video",
+                localId: "93489802",
+                mediaType: "video",
+                packetsLost: 0,
+                packetsReceived: 97,
+                roundTripTime: 0.026,
+                roundTripTimeMeasurements: 40,
+                ssrc: 3647127630,
+                timestamp: undefined,
+                totalRoundTripTime: 0.776,
+                type: "remote-inbound-rtp"
+              } */
+            } else if (kind === "audio") {
+              setAudioRTT(roundTripTime)
+              /* stats contains: {
+                codecId: "ce84b808",
+                fractionLost: 0,
+                id: "1c3c052d",
+                jitter: 0.004,
+                kind: "audio",
+                localId: "e9eea0d0",
+                mediaType: "audio",
+                packetsLost: 0,
+                packetsReceived: 1191,
+                roundTripTime: 0.021,
+                roundTripTimeMeasurements: 20,
+                ssrc: 342435103,
+                timestamp: undefined,
+                totalRoundTripTime: 0.376,
+                type: "remote-inbound-rtp"
+              } */
+            }
           }
         }
 
